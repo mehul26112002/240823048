fa2dd66dc8c527b57448d359a39374c7ad45f3c6
kanzariya mehul <mehulkanzariya26112002@gmail.com>
2025-07-15 11:09:09 +0530

    even odd program

---
+++ index.js
@@ -0,0 +1,18 @@
+// console.log("mehul kanzariya")
+
+
+
+// function add(a,b){
+//     sum=a+b;
+// }
+
+
+// 1.even odd
+const n = 5
+if(n%2)
+{
+    console.log("Odd Number")
+}else
+{
+    console.log("Even Number")
+}
